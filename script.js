ad11d7eed217abef329b71c6f108d52e67147953
annaaa9696 <annaniekrasova2701@gmail.com>
2025-11-25 14:25:55 +0100

    третьє збереження

--- script.js
+++ script.js
@@ -1,1 +1,52 @@
 
+// Функція для отримання значення кукі за ім'ям
+function getCookieValue(cookieName) {
+    // Розділяємо всі куки на окремі частини
+    const cookies = document.cookie.split(';');
+
+
+    // Шукаємо куки з вказаним ім'ям
+    for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].trim(); // Видаляємо зайві пробіли
+
+
+        // Перевіряємо, чи починається поточне кукі з шуканого імені
+        if (cookie.startsWith(cookieName + '=')) {
+            // Якщо так, повертаємо значення кукі
+            return cookie.substring(cookieName.length + 1); // +1 для пропуску символу "="
+        }
+    }
+    // Якщо кукі з вказаним іменем не знайдено, повертаємо порожній рядок або можна повернути null
+    return '';
+}
+
+
+let themeBtn = document.querySelector("#themeToggle")
+
+
+function setTheme(theme) {
+    if (theme == 'light') {
+        document.body.classList.add("light-theme");
+        themeBtn.innerHTML = '<i class="bi bi-moon"></i>';
+    } else {
+        document.body.classList.remove("light-theme");
+        themeBtn.innerHTML = '<i class="bi bi-brightness-high"></i>';
+    }
+}
+
+
+let theme = getCookieValue('theme')
+setTheme(theme)
+
+
+themeBtn.addEventListener("click", () => {
+    document.body.classList.toggle('light-theme'); // Перемикаємо клас теми
+    if (theme == 'light') {
+        theme = 'dark'
+    } else {
+        theme = 'light'
+    }
+    setTheme(theme)
+    // Зберігаємо JSON рядок у кукі
+    document.cookie = `theme=${theme}; max-age=${60 * 60 * 24 * 7}; path=/`;
+})
